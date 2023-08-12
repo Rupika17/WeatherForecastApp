11d52931167c3b570220d5d73076c4307d976a7e
Rupika17 <rupikasahariya@gmail.com>
2023-08-12 12:34:08 +0200

    renamed files

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { lightTheme } from './theme';
 import { ThemeProvider } from 'styled-components'; 
 
 
-import Home from './pages/Dashboard';
+import Dashboard from './pages/Dashboard';
 import { GlobalStyles } from './app.styled';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={lightTheme}>
       <GlobalStyles />
-      <Home />
+      <Dashboard />
     </ThemeProvider>
   );
 };
